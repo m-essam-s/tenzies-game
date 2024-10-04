cd2b38ccbe4c5512834dcded8b9d10a8464c180d
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-04 23:29:55 +0300

    Updated: Give some addtional information to the user

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,8 @@ const App = () => {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElement}
       </div>
