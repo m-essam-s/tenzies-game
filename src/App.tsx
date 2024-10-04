d1e397d4649e3c660bc2302d3492b108725194be
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-04 18:56:32 +0300

    Updated: Map array to Die components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import Die from './components/Die'
+import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice(): number[] {
+    return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))
+  }
+
+  const diceElement = dice.map((die, index) => {
+    return <Die key={index} value={die} />
+  })
+
+  const rollDice = () => {
+    setDice(allNewDice())
+  }
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElement}
       </div>
+
+      <button
+        className='roll-dice'
+        onClick={rollDice}
+      >Roll</button>
     </main>
   )
 }
